dc346b1ed818ad65cb7b25ec5e8565960b07cef0
Joseph  Benbella <benbellajoe@outlook.com>
2019-02-19 06:04:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // write your code below!
+function happyHolidays(){
+  return 'Happy holidays!';
+}
+
+function happyHolidaysTo (name){
+  return `Happy holidays, ${name}!`;
+}
+
+happyHolidaysTo('Bill');
+
+function happyHolidayTo (holiday, name){
+  return `Happy ${holiday}, ${name}!`;
+}
+
+happyHolidayTo('Christmas', 'Rick');
